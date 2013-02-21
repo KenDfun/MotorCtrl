356de9b9d76e5303a8a16850ddcf0dc47dd70c5e
Ken Okabe <okabe.kenichiro@gmail.com>
2013-02-21 17:52:23 +0900

    initial

---
+++ src/com/design_fun/motorctrl/MainActivity.java
@@ -0,0 +1,28 @@
+package com.design_fun.motorctrl;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		TextView tview = (TextView)findViewById(R.id.textView1);
+		tview.setText("ìÆìIÇ…ïœçX\n");
+		tview.append("Li\nne2\n");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
+
